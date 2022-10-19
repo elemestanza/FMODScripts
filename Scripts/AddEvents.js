754e5707b284a7f687d82e35c2ee08eba393cb65
Elena Mestanza <elemestanzar@gmail.com>
2022-10-19 19:53:20 +0200

    New version 1.03

--- Scripts/AddEvents.js
+++ Scripts/AddEvents.js
@@ -1,8 +1,9 @@
 /* -------------------------------------------
    FMOD Studio Script:
-   Add Events in a Folder from a String or a File
+   Add Events in a Folder from a String or a name or a File
+   - Could create a subfolder for the new events
    Author: Elena Mestanza (github.com/elemestanza)
-   Version: 1.02
+   Version: 1.03
    -------------------------------------------
  */
 
@@ -13,6 +14,16 @@ studio.menu.addMenuItem({
         return folder && folder.isOfExactType("EventFolder");
     },
     execute: function() {
+        var folder = studio.window.browserCurrent();
+        var createFolder = studio.system.question("Do you want to create a subfolder?")
+        if (createFolder){
+            var subfolderName = studio.system.getText("Subfolder name","Folder");
+            var subfolder = studio.project.create("EventFolder");
+            subfolder.name = subfolderName;
+            subfolder.folder = folder;
+            folder = subfolder;
+        }
+        
         var listString = studio.system.getText("List of new events", "Event 1, Event 2...");
 
         var isDataInLines = studio.system.question("Is your oeiginal data sort out by lines? I.e., copied from an Excel file");
@@ -22,7 +33,6 @@ studio.menu.addMenuItem({
 
         var eventList = listString.split(separator);
 
-        var folder = studio.window.browserCurrent();
         for (var i = 0; i < eventList.length; i++) {
             var newEvent = studio.project.create("Event");
             var track = studio.project.create("GroupTrack");
@@ -36,16 +46,25 @@ studio.menu.addMenuItem({
 });
 
 studio.menu.addMenuItem({
-    name: "Add Events\\From a Name (e.g. event_1, event_2...)",
+    name: "Add Events\\From a Name (e.g. \"event\" generates event_1, event_2...)",
     isEnabled: function() {
         var folder = studio.window.browserCurrent();
         return folder && folder.isOfExactType("EventFolder");
     },
     execute: function() {
+        var folder = studio.window.browserCurrent();
+        var createFolder = studio.system.question("Do you want to create a subfolder?")
+        if (createFolder){
+            var subfolderName = studio.system.getText("Subfolder name","Folder");
+            var subfolder = studio.project.create("EventFolder");
+            subfolder.name = subfolderName;
+            subfolder.folder = folder;
+            folder = subfolder;
+        }
+
         var name = studio.system.getText("Event name", "event");
         var numberOfEvents = studio.system.getNumber("Number of events", 2);
 
-        var folder = studio.window.browserCurrent();
         for (var i = 1; i <= numberOfEvents; i++) {
             var newEvent = studio.project.create("Event");
             var track = studio.project.create("GroupTrack");
@@ -65,6 +84,16 @@ studio.menu.addMenuItem({
         return folder && folder.isOfExactType("EventFolder");
     },
     execute: function() {
+        var folder = studio.window.browserCurrent();
+        var createFolder = studio.system.question("Do you want to create a subfolder?")
+        if (createFolder){
+            var subfolderName = studio.system.getText("Subfolder name","Folder");
+            var subfolder = studio.project.create("EventFolder");
+            subfolder.name = subfolderName;
+            subfolder.folder = folder;
+            folder = subfolder;
+        }
+
         var filePath = studio.system.getText("File's absolute path", "");
 
         var isDataInLines = studio.system.question("Is your data sort out by lines?");
@@ -76,7 +105,6 @@ studio.menu.addMenuItem({
         file.open(studio.system.openMode.ReadOnly);
         var eventList = file.readText(8192).split(separator);
 
-        var folder = studio.window.browserCurrent();
         for (var i = 0; i < eventList.length; i++) {
             var newEvent = studio.project.create("Event");
             var track = studio.project.create("GroupTrack");
